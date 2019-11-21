619d7ea33b7d9ea4a4e2d4b60e2638a5a132757c
fengandzhy <fengandzhy@gmail.com>
2019-11-21 20:39:58 +1300

    assignment

---
+++ python/src/chapter03/assignment.py
@@ -0,0 +1,21 @@
+'''
+Created on 2019年11月21日
+
+@author: Ellite Service
+'''
+x,y = 1,2
+print(x)
+print(y)
+print('============')
+x,y = y,x 
+print(x)
+print(y)
+
+x +=1
+print(x)
+x *=2
+print(x)
+x //=3
+print(x)
+
+
