030dc4c0170b32228d1ff9210e3e68526e767d82
fengandzhy <fengandzhy@gmail.com>
2019-11-24 21:42:14 +1300

    conditionClause

--- python/src/chapter03/conditionClause.py
+++ python/src/chapter03/conditionClause.py
@@ -3,3 +3,13 @@ Created on 2019年11月23日
 
 @author: Ellite Service
 '''
+n = 6
+if n ==3 :
+    print('n==3')
+elif n == 4:
+    print('n==4')
+elif n==5:
+    print('n==5')
+else : 
+    print('n什么都不是')
+
