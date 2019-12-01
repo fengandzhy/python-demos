502af0ee64cd006e967bf7bdab9d0b04dd5d35e9
fengandzhy <fengandzhy@gmail.com>
2019-12-02 09:11:21 +1300

    forloop

---
+++ python/src/chapter03/forloop.py
@@ -0,0 +1,15 @@
+'''
+Created on 2019年12月2日
+
+@author: Ellite Service
+'''
+numbers = [1,2,3,4]
+for num in numbers:
+    print(num,end=" ")
+    
+for i in range(0,10):
+    print(i)
+
+names = ['Bill','Sam','Duncan']
+for i in range(0,len(names)):
+    print(names[i])
