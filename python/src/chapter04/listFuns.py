65016e068d13c0fb4ce74753b600bfc2f8319a09
fengandzhy <fengandzhy@gmail.com>
2019-12-14 10:27:30 +1300

    listFuns

---
+++ python/src/chapter04/listFuns.py
@@ -0,0 +1,14 @@
+'''
+Created on 2019年12月14日
+
+@author: Ellite Service
+'''
+number = [1,2,3,4,5,6,7]
+print(len(number))
+print(max(number))
+print(min(number))
+
+#max 和 min 只能用在数值之间的比较
+array1=['2',3]
+# print(max(number))
+# print(min(number))
