4789c0a26a964b6a2f59ee9e23d70d7a7d3f4ef5
fengandzhy <fengandzhy@gmail.com>
2019-12-30 07:25:44 +1300

    formatString

--- python/src/chapter05/formatStringByFormat2.py
+++ python/src/chapter05/formatStringByFormat2.py
@@ -3,5 +3,22 @@
 #  100,000,000,000
 '''
 
+#字符串左对齐，数字右对齐
+str1 = '{num:12}'
+print(str1.format(num='3.1415926')) 
+
+str1 = '{num:12}'
+print(str1.format(num=3.1415926)) 
+
+
+#带有精度的
+str1 = '{num:12.2f}'
+print(str1.format(num=3.1415926))
+
+str1 = '{num:012.2f}'
+print(str1.format(num=3.1415926))
+
+str1 = '{num:012.2f}'
+print(str1.format(num=3.1415926))
 
 
