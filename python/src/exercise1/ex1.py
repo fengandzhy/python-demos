97ba045ef23b0753ffbde1026b8382f1c22ae606
fengandzhy <fengandzhy@gmail.com>
2020-01-01 20:31:02 +1300

    exercise1

--- python/src/exercise1/ex1.py
+++ python/src/exercise1/ex1.py
@@ -6,5 +6,9 @@ Created on 2020年1月1日
 numbers = []
 index = 1
 while index<=100 :
-    if index%3==0 && index%5==0 :
+    if index%3==0 :
+        if index%5==0 :
+            numbers.append(index)        
+    index = index +1
+print(numbers)
         
