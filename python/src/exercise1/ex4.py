e99f19cfec1901d00cd9ece97aeb560c3bc53010
fengandzhy <fengandzhy@gmail.com>
2020-01-02 21:39:43 +1300

    ex4

--- python/src/exercise1/ex4.py
+++ python/src/exercise1/ex4.py
@@ -3,3 +3,16 @@ Created on 2020年1月2日
 
 @author: Ellite Service
 '''
+strIn = str(input("请输入字符串:"))
+a1 = strIn.find("not")
+a2 = strIn.find("bad")
+
+if a1!=-1 and a2!=-1:
+    if a1 < a2 :
+        s1 = strIn[0:a1]
+        s2 = strIn[a2+3:len(strIn)]
+        strIn = s1+'good'+s2
+print(strIn)
+        
+
+
