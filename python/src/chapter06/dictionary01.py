0496bd3cb5fcdf35501dd25d0504d5fcb890ef45
fengandzhy <fengandzhy@gmail.com>
2020-01-04 23:12:38 +1300

    python dictionary

--- python/src/chapter06/dictionary01.py
+++ python/src/chapter06/dictionary01.py
@@ -5,8 +5,15 @@ python初始化字典有两种方法
 
 
 '''
-dict1 = {'name','value'}
+#简单创建一个字典
+dict1 = {'name':'value'}
 print(dict1)
+
+
+#元祖可以为key
+'''
+元组可以作为 dict 的 key，但列表不能作为元组的 key。这是由于 dict 要求 key 必须是不可变类型，但列表是可变类型，因此列表不能作为元组的 key
+'''
 dict2 = {(2,3):'abc',3:'def'}
 print(dict2)
 
