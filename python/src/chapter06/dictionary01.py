d3487839c094180e40c30ab4b8162fb909128d00
fengandzhy <fengandzhy@gmail.com>
2020-01-05 23:24:11 +1300

    dictionary define

--- python/src/chapter06/dictionary01.py
+++ python/src/chapter06/dictionary01.py
@@ -1,9 +1,4 @@
 '''
-python初始化字典有两种方法
-1. 大括号直接定义 info = {'name','value'}
-2. 用dict函数将一个序列转换为字典,但是这个序列的长度必须是2或者必须是由长度为2的子序列组成4
-
-
 '''
 #简单创建一个字典
 dict1 = {'name':'value'}
@@ -12,10 +7,32 @@ print(dict1)
 
 #元祖可以为key
 '''
-元组可以作为 dict 的 key，但列表不能作为元组的 key。这是由于 dict 要求 key 必须是不可变类型，但列表是可变类型，因此列表不能作为元组的 key
+元组可以作为 dict 的 key，但列表不能作为元组的 key。
+这是由于 dict 要求 key 必须是不可变类型，但列表是可变类型，因此列表不能作为元组的 key
 '''
 dict2 = {(2,3):'abc',3:'def'}
 print(dict2)
+
+'''
+将一个列表转换成字典
+'''
+vegetables = [('celery', 1.58), ('brocoli', 1.29), ('lettuce', 2.19)]
+dict3 = dict(vegetables)
+print(dict3)
+
+'''
+将一个元祖转换成字典
+'''
+vegetables = (('celery', 1.58), ('brocoli', 1.29), ('lettuce', 2.19))
+dict4 = dict(vegetables)
+print(dict4)
+
+'''
+列表，元祖，必须由长度为2的子列表或者子元祖组成
+'''
+cars = [['BMW', 8.5], ['BENS', 8.3], ['AUDI', 7.9]]
+dict5 = dict(cars)
+print(dict5)
 
 items = [['Bill',"4321"],("Mike","7891"),['John','4567']]
 d = dict(items)
