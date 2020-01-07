b043acae304705026dafd9a7759c0be9511a596c
fengandzhy <fengandzhy@gmail.com>
2020-01-07 23:34:37 +1300

    add elements to dictionary

---
+++ python/src/chapter06/dictionary02.py
@@ -0,0 +1,20 @@
+'''
+Created on 2020年1月7日
+
+@author: Ellite Service
+'''
+
+'''
+直接取值根据key
+'''
+score={'语文':89}
+print(score['语文'])
+
+'''
+直接设置值,哪怕之前没有的值也可以直接设置
+'''
+score['数学'] = 83
+print(score)
+
+score['语文'] = 86
+print(score)
