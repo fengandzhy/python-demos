aa69f44646b0bb68beda29fc96f66335ddca6f40
fengandzhy <fengandzhy@gmail.com>
2020-01-09 19:26:24 +1300

    dictionary len

---
+++ python/src/chapter06/len.py
@@ -0,0 +1,11 @@
+'''
+Created on 2020年1月9日
+
+@author: Ellite Service
+'''
+dict1 = dict(name='abc',value='def',attribute='add')
+print(len(dict1))
+
+dict2 = {'name':'abc','value':'def','attribute':'add'}
+print(len(dict2))
+
