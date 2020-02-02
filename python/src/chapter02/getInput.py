ace8df63f3c10ed3709e6be9e4f81ca6a40bc147
fengandzhy <fengandzhy@gmail.com>
2020-02-03 07:19:07 +1300

    python add

---
+++ python/src/chapter02/getInput.py
@@ -0,0 +1,14 @@
+'''
+Created on 2020年2月3日
+
+@author: Ellite Service
+'''
+
+name = input('请输入姓名:')
+age = int(input('请输入年龄:'))
+salary = float(input('请输入收入：'))
+
+print('name:',name)
+print('age:',age)
+print('salary:',format(salary, '0.2f'))
+
---
+++ python/src/chapter02/innerFunction.py
@@ -0,0 +1,15 @@
+'''
+Created on 2020年2月3日
+
+@author: Ellite Service
+'''
+# 内建函数
+# 通过模块提供的函数
+# 自定义的函数
+print(pow(3,3))
+print(abs(-2)) #幂运算
+print(round(3.2126,3)) #四舍五入小数点后保留3位
+from cmath import sin
+print(sin(3.14))
+
+
