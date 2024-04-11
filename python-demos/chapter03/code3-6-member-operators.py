908b43364d5f2d974de41daf77e9c9b293f335de
fengandzhy <fengandzhy@gmail.com>
2024-04-11 20:43:57 +1200

    chapter02 code3-6-member-operators.py

---
+++ python-demos/chapter03/code3-6-member-operators.py
@@ -0,0 +1,17 @@
+print('12' in '123')
+print('hi' not in 'hello')
+a = 1
+b = 2
+print(a is b)
+print(a is not b)
+
+my_list = [1, 2, 3, 4, 5]
+print(3 in my_list)  # 输出 True
+
+# 检查字典中是否存在指定键
+my_dict = {'a': 1, 'b': 2, 'c': 3}
+print('b' in my_dict)  # 输出 True
+
+# 检查字符串中是否存在指定子串
+my_string = "Hello, world!"
+print('world' in my_string)
